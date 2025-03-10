3eb48127bfa48476a93332b61bc57bfd3d031d2d
MarcoProstamo <Marcopros1201@gmail.com>
2025-03-10 21:51:46 +0100

    Add → Destroy

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -8,26 +8,17 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $projects = Project::all();
         return view("projects.index", compact("projects"));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view("projects.create");
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $newProject = new Project();
@@ -38,9 +29,6 @@ class ProjectController extends Controller
         return redirect()->route("projects.show", $newProject);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Project $project)
     {
         return view("projects.show", compact("project"));
@@ -65,8 +53,9 @@ class ProjectController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Project $project)
     {
-        //
+        $project->delete();
+        return redirect()->route("projects.index");
     }
 }
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,5 +3,20 @@
 @section('title', 'Single Project')
 
 @section('content')
-    @dd($project)
+    <form action="{{ route('projects.destroy', $project) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <input type="submit" value="Delete Project" class="btn btn-danger mb-3">
+    </form>
+
+    <div class="card">
+        <div class="card-header">
+            <h3>{{ $project->project_name }}</h3>
+        </div>
+        <div class="card-body">
+            <p><strong>Client Name:</strong> {{ $project->client_name }}</p>
+            <p><strong>Project Description:</strong> {{ $project->project_description }}</p>
+        </div>
+    </div>
+    <a href="{{ route('projects.index') }}" class="btn btn-primary mt-2">Back to Projects</a>
 @endsection
