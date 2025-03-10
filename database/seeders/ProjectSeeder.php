d0b86d36e5e7badb515246307c5d480f7058a81f
MarcoProstamo <Marcopros1201@gmail.com>
2025-03-10 15:11:55 +0100

    Add → Routes + Auth on Projects Resource

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $newProject = new Project();
+            $newProject->project_name = $faker->unique()->word;
+            $newProject->client_name = $faker->name();
+            $newProject->project_description = $faker->paragraph();
+            $newProject->save();
+        }
+    }
+}
